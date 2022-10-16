b0aea4a3b0db8765cd6de79c9cfca46927721b39
miguelsolans <dev.miguelsolans@gmail.com>
2022-10-16 17:39:22 +0100

    + Temporary ViewController Background Color

--- ShowTime/Generics/BaseViewController.swift
+++ ShowTime/Generics/BaseViewController.swift
@@ -13,6 +13,7 @@ class BaseViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view
+        self.view.backgroundColor = .white;
         self.style();
         self.layout();
     }
