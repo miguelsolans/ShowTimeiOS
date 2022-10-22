e266a3f0871fa6f19a6a86c9aa442078a19e2d5f
miguelsolans <dev.miguelsolans@gmail.com>
2022-10-22 22:35:06 +0100

    + Generic Tables Support

---
+++ ShowTime/Business/Models/GenericTable/GenericTableConstants.swift
@@ -0,0 +1,12 @@
+//
+//  GenericTableConstants.swift
+//  ShowTime
+//
+//  Created by Miguel Solans on 22/10/2022.
+//
+
+import Foundation
+
+struct GenericTableConstants {
+    static let Countries = "Countries";
+}
